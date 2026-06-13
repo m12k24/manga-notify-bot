078456184ec62b8f0c5aeeb8b1eac6438e4b4007
m12k24 <meishiba1112@icloud.com>
2026-06-13 22:12:48 +0900

    java基礎学習

--- Main.java
+++ Main.java
@@ -1,21 +1,20 @@
 public class Main {
     public static void main(String[] args) {
-String today = "2024-07-01";
+        String today = "2026-06-13";
 
-        String[] mangaList = {"One Piece", "Naruto", "Bleach"};
-        String[] releaseDates = {"2024-07-01", "2024-08-15", "2024-09-10"};
+        String[] mangaList = {"ワンピース", "ナルト", "鬼滅の刃"};
+        String[] releaseDates = {"2026-06-13", "2026-07-01", "2026-06-13"};
 
-        for(int i = 0; i < mangaList.length; i++) {
+        for (int i = 0; i < mangaList.length; i++) {
             checkReleaseDate(today, mangaList[i], releaseDates[i]);
         }
     }
 
-    static void checkReleaseDate(String today, String manga, String releaseDate){
-        if(today.equals(releaseDate)){
-                System.out.println(manga + "は今日が発売日!");
-            }else {
-                System.out.println(manga + "はまだ発売日ではありません");
-            }
-
+    static void checkReleaseDate(String today, String manga, String releaseDate) {
+        if (today.equals(releaseDate)) {
+            System.out.println(manga + "は今日が発売日！");
+        } else {
+            System.out.println(manga + "はまだ発売日ではありません");
+        }
     }
 }
